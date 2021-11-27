00e1f164f7a5bb6ff5f2ca61b9de226cbb2015d1
Ivan Jimenez <chiklete@gmail.com>
2021-11-28 00:23:06 +0100

    Manejadores de errores

--- main.js
+++ main.js
@@ -13,10 +13,17 @@ const moviesRouter = require('./router/movies.router')
 
 server.use('/movies', moviesRouter)
 
-server.use('*', (req, res)=>{
-    res.status(404).json('Not Found*')
+server.use('*', (req, res, next)=>{
+    const error = new Error('Ruta no encontrada');
+    error.status = 404;
+    next(error);
 });
 
+// Manejador de errores, siempre se define con los 4 parametros
+server.use((err, req, res, next) => {
+    console.log('[ERROR] Ha ocurrido un error', err.status, err.message)
+	return res.status(err.status || 500).json(err.message || 'Se ha producido un error en el servidor');
+});
 
 server.listen(PORT, ()=>{
     console.log(`The server running in http://localhost/${PORT}`)
--- router/movies.router.js
+++ router/movies.router.js
@@ -6,7 +6,7 @@ const Movie = require('../models/Movie')
 
 
 // GET todas las peliculas
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   return Movie.find()
   .then(movies => {
       // Si encontramos las peliculas, los devolveremos al usuario
@@ -14,30 +14,31 @@ router.get('/', (req, res) => {
   })
   .catch((err)=>{
       // Si hay un error, enviaremos por ahora una respuesta de error.
-      console.log('Error en GET /', err)
-      return res.status(500).json('Ha ocurrido un error en el servidor')
+      const errorOcurrido = new Error();
+      return next(errorOcurrido)
   })
 })
 
 // GET pelicula por ID
-router.get('/:id',(req, res)=>{
+router.get('/:id',(req, res, next)=>{
   const id = req.params.id;
   Movie.findById(id)
     .then(movie => {
       if (!movie){
-        return res.status(404).json('La película no existe')
+        const error = new Error(`La pelicula con el ID ${id}, no existe `);
+        error.status = 404;
+        return next(error)
       }
       res.json(movie)
     })
     .catch((err)=>{
-      console.log(`Error en GET /${id}`, err)
-      return res.status(500).json('Ha habido un error en el servidor')
+      return next(new Error())
     })
 
 })
 
 // GET peliculas por su titulo
-router.get('/titulo/:titulo', (req, res)=>{
+router.get('/titulo/:titulo', (req, res, next)=>{
   const titulo = req.params.titulo;
   Movie.find({ title: titulo })
     .then(movies => {
@@ -46,13 +47,13 @@ router.get('/titulo/:titulo', (req, res)=>{
     })
     .catch((err)=>{
         // Si hay un error, enviaremos por ahora una respuesta de error.
-        console.log(`Error en GET /error/${titulo}`, err)
-        return res.status(500).json('Ha ocurrido un error en el servidor')
+        
+        return next(new Error())
     })
 })
 
 // GET peliculas por su genero
-router.get('/genero/:genero', (req, res)=>{
+router.get('/genero/:genero', (req, res, next)=>{
   const genero = req.params.genero;
   Movie.find({ genre: genero })
     .then(movies => {
@@ -62,12 +63,12 @@ router.get('/genero/:genero', (req, res)=>{
     .catch((err)=>{
         // Si hay un error, enviaremos por ahora una respuesta de error.
         console.log(`Error en GET /genero/${genero}`, err)
-        return res.status(500).json('Ha ocurrido un error en el servidor')
+        return next(new Error())
     })
 })
 
 // GET peliculas estrenadas a partir de la fecha indicada
-router.get('/fecha/:fecha', (req, res)=>{
+router.get('/fecha/:fecha', (req, res, next)=>{
   const fecha = req.params.fecha;
   Movie.find({year:{$gt:fecha}})
     .then(movies => {
@@ -75,64 +76,12 @@ router.get('/fecha/:fecha', (req, res)=>{
     })
     .catch((err)=>{
       console.log(`Error en GET /fecha/${fecha}`, err)
-      return res.status(500).json('ha ocurrido un error en el servidor')
+      return next(new Error())
     })
 })
 
 
 
-
-
-// router.get('/movies',(req,res) => {
-//     res.send(movies)
-// })
-// router.get('/movies/:titulo', (req, res) => {
-//     let tituloPeli = req.params.titulo;
-//     const peliPorTitulo = movies.filter(tituloEnArray => tituloEnArray.title === tituloPeli);
-//     if (!peliPorTitulo){
-//         res.status(404).send('No se ha encontrado la peli')
-//         return;
-//     }
-
-//     res.send(peliPorTitulo)
-// })
-// router.get('/id/:id', (req, res) => {
-//     let idPeli = Number(req.params.id);
-//     if (Number.isNaN(idPeli)){
-//         res.sendStatus(400);
-//         return;
-//     }
-//     //const idPeliADevolver = movies.find(idEnElArray=>idEnElArray.id ===idPeli)
-//     idPeli = idPeli.toString()
-//     res.send(idPeli)
-// })
-// router.get('/genero/:genero', (req, res) => {
-//     let genPeli = req.params.genero;
-    
-//     const genderPeli = movies.filter(genderEnArray => genderEnArray.genre === genPeli)
-//     if (!genderPeli){
-//         res.status(404).send('El género no coincide con ninguno existente')
-//         return;
-//     }
-//     res.send(genderPeli)
-// })
-// router.get('/anos/:ano', (req, res) => {
-//     const anoPeli = Number(req.params.ano);
-//     if (Number.isNaN(anoPeli)){
-//         res.sendStatus(400)
-//         return;
-//     }
-
-//     const pelisMas2010 = movies.filter(pelisEnArray => pelisEnArray.year > anoPeli)
-//     res.send(pelisMas2010)
-
-// })
-
-
-// router.get('/peliculas/:titulo',(req, res) => {
-//     const tituloPeli =req.params.titulo;
-//     res.send('titulo: ',tituloPeli)
-// })
 
 
 module.exports = router;
